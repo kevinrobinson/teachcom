898ccd5cd4700b1ae6a1fcc1ecd4188393a3e687
Daniel Freiman <djfreiman@gmail.com>
2012-12-01 13:11:02 -0500

    adding twilio config

--- py/teachercom/teachercom/settings.py
+++ py/teachercom/teachercom/settings.py
@@ -33,7 +33,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_twilio',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -159,3 +160,5 @@ LOGGING = {
         },
     }
 }
+
+TWILIO_DEFAULT_CALLERID = '2155551515'
