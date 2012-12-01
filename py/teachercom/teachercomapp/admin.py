caa529e502276e2f96ed379fc75fa4889a9c00cb
Dan, on local <daniel.lopezbraus@gmail.com>
2012-12-01 13:15:46 -0500

    added admin view

---
+++ py/teachercom/teachercomapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from teachercomapp.models import Student, Message, Event
+
+admin.site.register(Student)
+admin.site.register(Message)
+admin.site.register(Event)
--- py/teachercom/teachercomapp/models.py
+++ py/teachercom/teachercomapp/models.py
@@ -1,25 +1,23 @@
 from django.db import models
 
 # Create your models here.
-class Students(models.Model):
-	student_id = models.BigIntegerField().unique 
-	first_name = models.CharField(max_length=30)
-	last_name = models.CharField(max_length=30)
-	sms_notification_ind = models.BooleanField()
-	call_notification_ind = models.BooleanField()
-	email_notification_ind = models.BooleanField()
-	phone_number = models.CharField(max_length=30)
-	email= models.CharField(max_length=30)
+class Student(models.Model):
+    student_id = models.BigIntegerField().unique 
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    sms_notification_ind = models.BooleanField()
+    call_notification_ind = models.BooleanField()
+    email_notification_ind = models.BooleanField()
+    phone_number = models.CharField(max_length=30)
+    email= models.CharField(max_length=30)
 
-class Messages(models.Model):
-	message_id = models.BigIntegerField()
-	text = models.TextField()
+class Message(models.Model):
+    text = models.TextField()
 
-class Events(models.Model):
-	event_id = models.BigIntegerField()
-	student_id = models.ForeignKey('Students')
-	message_id = models.ForeignKey('Messages')
-	date_of_message = models.DateTimeField()
-	time_of_message = models.DateTimeField()
-	type_of_message = models.IntegerField() #Create a dict for this
-	result_of_message = models.IntegerField() #and this
+class Event(models.Model):
+    student = models.ForeignKey('Student')
+    message = models.ForeignKey('Message')
+    date_of_message = models.DateTimeField()
+    time_of_message = models.DateTimeField()
+    type_of_message = models.IntegerField() #Create a dict for this
+    result_of_message = models.IntegerField() #and this
