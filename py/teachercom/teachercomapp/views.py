20595a9a24ac6afff2f58016e0e60fc6af30cd95
Graham <gcs272@gmail.com>
2012-12-02 12:47:53 -0500

    add many:many relationships between students and teachers

--- py/teachercom/teachercomapp/views.py
+++ py/teachercom/teachercomapp/views.py
@@ -3,7 +3,7 @@ from django.core.context_processors import csrf
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.views.decorators.cache import cache_page
-from teachercomapp.models import Student, Message, Event
+from teachercomapp.models import Student, Message, Event, Teacher
 import datetime
 import csv
 import StringIO
@@ -16,9 +16,10 @@ def index(request):
 
 def send(request):
     if request.method == 'GET':
+        teacher = Teacher.objects.get(user=request.user)
         data = {
-            'students': Student.objects.order_by('first_name'),
-            'messages': Message.objects.order_by('label')
+            'students': Student.objects.filter(teachers=teacher).order_by('first_name'),
+            'messages': Message.objects.filter(teacher=teacher).order_by('label')
         }
 
         data.update(csrf(request))
@@ -52,6 +53,8 @@ def handle_csv(request):
         if request.POST['skip_first_line'] == 'on':
             next(fp)
 
+        teacher = Teacher.objects.get(user=request.user)
+
         reader = csv.reader(fp)
         for row in reader:
             student = Student(
@@ -62,13 +65,12 @@ def handle_csv(request):
                 email = row[4].strip(),
                 sms_notification_ind = (row[5].strip() == 'True'),
                 call_notification_ind = (row[6].strip() == 'True'),
-                email_notification_ind = (row[7].strip() == 'True')
+                email_notification_ind = (row[7].strip() == 'True'),
             )
             student.save()
+            student.teachers.add(teacher)
+            student.save()
         return render_to_response('csv-saved.html')
-
-        
-
 
 def send_message(student, message, message_type):
     print 'sending message for %s' % (student.first_name)
