4804772d09af291f07d71dd47bbad210fb41df47
Dan, on local <daniel.lopezbraus@gmail.com>
2012-12-02 10:51:11 -0500

    user registration should be workign

--- py/teachercom/teachercom/settings.py
+++ py/teachercom/teachercom/settings.py
@@ -146,7 +146,7 @@ INSTALLED_APPS = (
 #Number of days for activation email to stay valid
 ACCOUNT_ACTIVATION_DAYS = 7
 
-SEND_EMAIL = True  #Change this to True to require 2 step activation with an email
+SEND_EMAIL = False  #Change this to True to require 2 step activation with an email
 
 #Temporary email backend 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
